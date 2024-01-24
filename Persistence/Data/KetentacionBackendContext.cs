01693d5bdb321cc37f8a282a2a8bd8520ac11194
Daniel Felipe Diaz Sanchez <137439716+Daniel12082@users.noreply.github.com>
2024-01-24 18:11:37 -0500

    Revert "Pull request para enviar mi info "

--- Persistence/Data/KetentacionBackendContext.cs
+++ Persistence/Data/KetentacionBackendContext.cs
@@ -18,26 +18,9 @@ public class KetentacionBackendContext : DbContext
     public DbSet<UserRol> UsersRols { get; set; }
     public DbSet<Rol> Rols { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
-    public DbSet<Address> Addresses { get; set; }
-    public DbSet<Company> Companies { get; set; }
-    public DbSet<CategoryItem> CategoryItems { get; set; }
-    public DbSet<CategoryProduct> CategoryProducts { get; set; }
-    public DbSet<City> Cities { get; set; }
-    public DbSet<Client> Clients { get; set; }
-    public DbSet<Country> Countries { get; set; }
-    public DbSet<Department> Departments { get; set; }
-    public DbSet<Event> Events { get; set; }
-    public DbSet<Invoice> Invoices { get; set; }
-    public DbSet<Item> Items { get; set; }
-    public DbSet<Notification> Notifications { get; set; }
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<PaymentMethod> PaymentMethods { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<ProductItem> ProductItems { get; set; }
-    public DbSet<SalesInvoice> SalesInvoices { get; set; }
-    public DbSet<Supplier> Suppliers { get; set; }
-    public DbSet<TypeSupplier> TypeSuppliers { get; set; }
-
+    public DbSet<Address> Address { get; set; }
+    public DbSet<Company> Company { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
